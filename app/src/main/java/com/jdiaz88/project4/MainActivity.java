069402f38f4a40852a4e2409621bce0f5a647a5a
Jacob Diaz <hello@jacobdiaz.dev>
2021-04-09 16:17:38 -0500

    Implemented working threads to logcat

--- app/src/main/java/com/jdiaz88/project4/MainActivity.java
+++ app/src/main/java/com/jdiaz88/project4/MainActivity.java
@@ -4,19 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+// Resources: https://stackoverflow.com/questions/17472572/execute-two-threads-which-wait-one-for-the-other-while-main-thread-continues
+// https://stackoverflow.com/questions/15530484/how-to-switch-between-two-thread-back-and-forth
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private final Handler ticHandler = new Handler(Looper.myLooper());
     private int playerOneScoreCount, playerTwoScoreCount, roundCount;
     private TextView playerOneScoreTv, playerTwoScoreTv, playerWinningTv;
     private final Button[] cells = new Button[9];
-    private Button resetGameBtn;
-    boolean isRunning;
+    private Button resetGameBtn, runThreadBtn;
+    boolean isRunning, isThreadGame; // Is thread game describes if it is a game against threads or a game against players
     boolean activePlayer; // player1 = 0 : player2 = 1
 
     // GameState -> (Player 1 = 0) (Player 2 = 1) (empty = 2)
@@ -42,9 +47,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         playerTwoScoreTv = (TextView) findViewById(R.id.playerTwoScore);
         playerWinningTv = (TextView) findViewById(R.id.playerWinning);
         resetGameBtn = (Button) findViewById(R.id.btn_ResetGame);
-
+        runThreadBtn = (Button) findViewById(R.id.btn_StartThreads);
         resetGameBtn.setOnClickListener(this::onResetBtnClicked);
-
+        runThreadBtn.setOnClickListener(this::onRunThreadBtnClicked);
         // Initialize Grid Cells
         initGrid();
 
@@ -54,21 +59,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         playerOneScoreCount = 0;
         playerTwoScoreCount = 0;
         isRunning = true;
+        isThreadGame = false; // Your able to play the game if you dont click run threads
         activePlayer = true;
         roundCount = 0;
     }
 
 
+// Game Logic and UI
 
 
     // Reset the game board
     public void onResetBtnClicked(View v) {
-        roundCount = 0;
-        activePlayer = true;
-        isRunning = true;
-        for (int i = 0; i < cells.length; i++) {
-            gameState[i] = 2;
-            cells[i].setText("");
+        resetGame();
+    }
+
+    public void onRunThreadBtnClicked(View v) {
+        Thread t1 = new Thread(new ThreadOne());
+        Thread t2 = new Thread(new ThreadTwo());
+        t1.start();
+        t2.start();
+    }
+
+    public synchronized void incrementCount() {
+        roundCount++;
+    }
+
+    public static class GlobalClass {
+        public static boolean isThread1done = false;
+        public static boolean isThread2done = false;
+    }
+
+    public class ThreadOne implements Runnable {
+        public void methodAandB() {
+                for (int i = 0; i < 5; i++) {
+                    while (GlobalClass.isThread1done) {
+                        //Do something
+                    }
+                    if (roundCount != 9) {
+                        waitSeconds(1);
+                        Log.i("test", "Thread (1): Inc round: " + roundCount);
+                        incrementCount();
+                    GlobalClass.isThread1done = true;
+                    GlobalClass.isThread2done = false;
+                }
+            }
+        }
+        @Override
+        public void run() {
+            methodAandB();
+        }
+    }
+
+    public class ThreadTwo implements Runnable {
+        public void methodAorB() {
+                for (int i = 0; i < 5; i++) {
+                    while (GlobalClass.isThread2done) {
+                        // Do Something
+                    }
+                    if (roundCount != 9) {
+                        waitSeconds(1);
+                        Log.i("test", "Thread (2): Inc round: " + roundCount);
+                        incrementCount();
+                        GlobalClass.isThread2done = true;
+                        GlobalClass.isThread1done = false;
+                    }
+                }
+        }
+
+        @Override
+        public void run() {
+            methodAorB();
         }
     }
 
@@ -76,7 +136,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         Button currentCell = (Button) v;
-        while (isRunning) {
+        while (isRunning && !isThreadGame) { // While playable and not a thread game
             // If the cell has already been clicked -> return
             if (!((Button) v).getText().toString().equals("")) {
                 return;
@@ -107,14 +167,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
 
             // If tie
-            if (roundCount == cells.length) {
+            else if (roundCount == cells.length) {
                 playerWinningTv.setText("Tie!");
             }
-
         }
     }
 
-    // initalize the grid
+    // Populate cells array and set listeners
     public void initGrid() {
         for (int i = 0; i < cells.length; i++) {
             String cellID = "cell_" + i;
@@ -137,10 +196,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     gameState[winningPosition[0]] != 2) {
                 result = true;
                 updateScores();
-                isRunning=false;
+                isRunning = false;
             }
         }
-        logBoard();
+//        logBoard();
 
         return result;
     }
@@ -163,6 +222,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    // Reset the board
+    public void resetGame() {
+        roundCount = 0;
+        activePlayer = true;
+        isRunning = true;
+        playerWinningTv.setText("");
+        for (int i = 0; i < cells.length; i++) {
+            gameState[i] = 2;
+            cells[i].setText("");
+        }
+    }
+
     // Helper functions
     public void logBoard() {
         String result = "\n";
@@ -178,5 +249,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         Log.i("test", "Board:\n\n" + result);
     }
+
+    public void waitSeconds(long secondsToWait) {
+        try {
+            Thread.sleep(secondsToWait * 1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
+// 0 1 2 3 4 5 6 7 8 Picks(4)
+// 0 1 2 3 5 6 7 8 Picks(3)
+// 0 1 2 5 6 7 8 Picks (2)
+// 0 1 5 6 7 8 Picks(8)
+// 0 1 5 6 7 Picks(0)
+// 1 5 6 7
 
